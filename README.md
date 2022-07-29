f966910d141a780488e3156c20ee0b870dacc8a3
Jatin Sanjay Lakhorkar <jatin.lakhorkar@gmail.com>
2022-07-29 13:14:47 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# kind-rock-0084f9110
+# FaQ ChatBot
+This is <b>FaQ website</b> which answers only basic questions stored in Knowledge base.</br>
+I have scraped only two website which is based on e-commerce and can only answer those questions.</br>
+1.<a href="https://hytest.fi/information/online-store-faq">Hytest online-store-faq</a></br>
+2.<a href="https://www.croma.com/faq">Croma FaQs</a>
+<h4><a href="https://www.awesomescreenshot.com/video/10208924?key=2d853e7f402ca2fdb5b985e21e95cb96">Video Link </a></h4>
+
+# Microsoft Azure Technologies Used 
+1).QnA Maker</br>
+2).App Services</br>
+3).Web Apps</br>
---
+++ static/scripts/chat.js
@@ -0,0 +1,36 @@
+//faq page
+var faq = document.getElementsByClassName("faq-page");
+var i;
+
+for (i = 0; i < faq.length; i++) {
+    faq[i].addEventListener("click", function () {
+        /* Toggle between adding and removing the "active" class,
+        to highlight the button that controls the panel */
+        this.classList.toggle("active");
+
+        /* Toggle between hiding and showing the active panel */
+        var body = this.nextElementSibling;
+        if (body.style.display === "block") {
+            body.style.display = "none";
+        } else {
+            body.style.display = "block";
+        }
+    });
+}
+// Collapsible
+var coll = document.getElementsByClassName("collapsible");
+
+for (let i = 0; i < coll.length; i++) {
+    coll[i].addEventListener("click", function () {
+        this.classList.toggle("active");
+
+        var content = this.nextElementSibling;
+
+        if (content.style.maxHeight) {
+            content.style.maxHeight = null;
+        } else {
+            content.style.maxHeight = content.scrollHeight + "px";
+        }
+
+    });
+}
